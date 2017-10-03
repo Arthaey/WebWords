c116f69e6c18e4089239839c8061bbe4eec33807
Arthaey <arthaey@gmail.com>
2017-10-02 19:18:03 -0700

    Fix misnamed test.

--- spec/ConstantsSpec.js
+++ spec/ConstantsSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe("Language", function() {
+describe("Constants", function() {
   it("constructor does nothing", function() {
     const constants = new Constants();
     expect(constants).not.toBeUndefined();
